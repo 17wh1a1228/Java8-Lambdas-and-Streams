138173ac900718ea6e0aaca26b5bba283b7faced
17wh1a1228 <42312840+17wh1a1228@users.noreply.github.com>
2020-07-21 16:58:21 +0530

    Update Average.java

--- java8_lambdas_streams/src/main/java/com/maven/java8_lambdas_streams/Average.java
+++ java8_lambdas_streams/src/main/java/com/maven/java8_lambdas_streams/Average.java
@@ -6,12 +6,11 @@ public class Average {
 	
     public static void main( String[] args ) {
     	Scanner sc = new Scanner(System.in);
-		int n= sc.nextInt();
+	int n= sc.nextInt();
     	List<Integer> list = new ArrayList<>();
-    	for(int i=0;i<n;i++)
-		{
-			list.add(sc.nextInt());
-		}
+    	for(int i=0;i<n;i++) {
+		list.add(sc.nextInt());
+	}
         System.out.println( "Average of a list of Integers : " + findAverage(list));
         sc.close();
     }
