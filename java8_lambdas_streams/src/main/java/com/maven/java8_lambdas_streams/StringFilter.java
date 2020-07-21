a18833f06989736ce33e02d213517bc78073b9bf
17wh1a1228 <42312840+17wh1a1228@users.noreply.github.com>
2020-07-21 17:00:37 +0530

    Update StringFilter.java

--- java8_lambdas_streams/src/main/java/com/maven/java8_lambdas_streams/StringFilter.java
+++ java8_lambdas_streams/src/main/java/com/maven/java8_lambdas_streams/StringFilter.java
@@ -8,15 +8,15 @@ public class StringFilter {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n= sc.nextInt();
-    	List<String> list = new ArrayList<>();
-    	for(int i=0;i<n;i++)
-		{
+		List<String> list = new ArrayList<>();
+		for(int i=0;i<n;i++) {
 			list.add(sc.next());
 		}
-        System.out.println(filterStrings(list));
-        sc.close();
-    }
-    static List<String> filterStrings(List<String> list) {
-    	 return list.stream().filter(s -> s.startsWith("a") && s.length() == 3).collect(Collectors.toList());
+		System.out.println(filterStrings(list));
+		sc.close();
+	    }
+	
+	static List<String> filterStrings(List<String> list) {
+		return list.stream().filter(s -> s.startsWith("a") && s.length() == 3).collect(Collectors.toList());
 	}
 }
