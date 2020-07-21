59e9aa43c79cb005fd3854f537aa9ed191b3454f
17wh1a1228 <42312840+17wh1a1228@users.noreply.github.com>
2020-07-21 16:59:24 +0530

    Update Palindromes.java

--- java8_lambdas_streams/src/main/java/com/maven/java8_lambdas_streams/Palindromes.java
+++ java8_lambdas_streams/src/main/java/com/maven/java8_lambdas_streams/Palindromes.java
@@ -8,15 +8,14 @@ public class Palindromes {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n= sc.nextInt();
-    	List<String> list = new ArrayList<>();
-    	for(int i=0;i<n;i++)
-		{
+		List<String> list = new ArrayList<>();
+		for(int i=0;i<n;i++) {
 			list.add(sc.next());
 		}
-        System.out.println(palindromeStrings(list));
-        sc.close();
-    }
-    static List<String> palindromeStrings(List<String> list) {
-    	 return list.stream().filter(s -> s.equals(new StringBuffer(s).reverse().toString())).collect(Collectors.toList());
+		System.out.println(palindromeStrings(list));
+		sc.close();
+	}
+    	static List<String> palindromeStrings(List<String> list) {
+    		return list.stream().filter(s -> s.equals(new StringBuffer(s).reverse().toString())).collect(Collectors.toList());
 	}
 }
